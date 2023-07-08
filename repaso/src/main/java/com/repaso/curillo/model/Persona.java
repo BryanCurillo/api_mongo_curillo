0964974ab81d4c1d958ddf7096dcac3507177544
BryanCurillo <102774552+BryanCurillo@users.noreply.github.com>
2023-07-07 21:44:57 -0500

    Add files via upload

---
+++ repaso/src/main/java/com/repaso/curillo/model/Persona.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.repaso.curillo.model;
+
+import java.util.List;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ *
+ * @author Bryan
+ */
+@Document(collection = "Persona")
+@Data
+public class Persona {
+
+    @Id
+    private Long id;
+    
+    private String nombre;
+    
+    private String apellido;    
+    
+    private String direccion;
+    
+    private Contactos contactos;
+    
+    private List<Telefonia> listaTelefonia;
+
+}
